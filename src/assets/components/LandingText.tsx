538020cae8346ae1ffbf4f6f3fdc723c1ac16bcb
banaz <banazsinjary@gmail.com>
2023-07-04 15:39:53 -0700

    text arrangement

--- src/assets/components/LandingText.tsx
+++ src/assets/components/LandingText.tsx
@@ -5,9 +5,11 @@ import SwitchText from "./SwitchText";
 function LandingText(){
     return(
         <>
+        <div id="landing-text">
         <p>Hey I'm Banaz</p>
         <p>I'm a blend of App Designer, Developer, </p>
         <p>{<SwitchText></SwitchText>}</p>
+        </div>
         </>
     )
 }
