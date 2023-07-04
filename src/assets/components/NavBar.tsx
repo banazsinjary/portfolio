f83001a7dd205c56ae033a75080d2240611653a7
banaz <banazsinjary@gmail.com>
2023-07-04 14:35:33 -0700

    nav bar spacing, font, links

--- src/assets/components/NavBar.tsx
+++ src/assets/components/NavBar.tsx
@@ -1,9 +1,32 @@
+import "./Components.css";
+
 function NavBar() {
   return (
     <>
-      <div className="nav-links"></div>
+      <div id="nav-container">
+        <div className="nav-links">
+          <ul>
+            <li>
+              <a href="https://www.linkedin.com/in/banazs/">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://github.com/banazsinjary/">GitHub</a>
+            </li>
+            <li>
+              <a href="mailto: banazsinjary@gmail.com">Email</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1qQHP4RSuHMQB4jOK9YBc0IEYyJiK0D7t/view?usp=sharing">
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
 
-      <div className="screen-mode"></div>
+        <div className="screen-mode">
+          <p>place hold</p>
+        </div>
+      </div>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import NavBar from './assets/components/NavBar.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NavBar></NavBar>
   </React.StrictMode>,
 )
