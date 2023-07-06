d72a7bb998d2b4522c28722cb38c114a7e2e4200
banaz <banazsinjary@gmail.com>
2023-07-06 16:44:22 -0700

    sort about me

--- src/assets/components/AboutMe.tsx
+++ src/assets/components/AboutMe.tsx
@@ -3,14 +3,24 @@ import "./Components.css";
 function AboutMe() {
   return (
     <>
-    <section className="section-sizing" id="about">
-      <div id="about-right">
+      <section className="section-sizing" id="about">
         <div id="about-right-title">A Little More About Me</div>
-        <div id="about-right-illustration">
-    
+        <div id="about-spacing">
+          <div id="about-right-illustration"></div>
+
+          <div id="about-left">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              pretium arcu vel nisl posuere tincidunt. Proin at odio vel dui
+              accumsan auctor. Mauris ullamcorper nisl ullamcorper orci interdum
+              porttitor. Mauris eget nisl id est vestibulum fermentum. Etiam
+              cursus tincidunt libero, et dictum eros pulvinar nec. Donec
+              ullamcorper dui quis interdum molestie. Etiam purus sem, rhoncus
+              et sollicitudin non, condimentum vitae lorem. Sed et commodo enim.
+              Duis ullamcorper lacus vel leo vehicula dictum.
+            </p>
+          </div>
         </div>
-      </div>
-      <div id="about-left"></div>
       </section>
     </>
   );
